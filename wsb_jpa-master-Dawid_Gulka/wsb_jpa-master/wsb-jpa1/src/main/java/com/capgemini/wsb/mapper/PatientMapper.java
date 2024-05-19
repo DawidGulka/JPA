7f4a29bedba9d914d5a6d5078587bd3762e0013a
DawidGulka <dawidgulkadc@wp.pl>
2024-05-20 00:56:35 +0200

    Add files via upload

---
+++ wsb_jpa-master-Dawid_Gulka/wsb_jpa-master/wsb-jpa1/src/main/java/com/capgemini/wsb/mapper/PatientMapper.java
@@ -0,0 +1,45 @@
+package com.capgemini.wsb.mapper;
+
+import com.capgemini.wsb.dto.PatientTO;
+import com.capgemini.wsb.persistence.entity.PatientEntity;
+import java.util.stream.Collectors;
+
+public class PatientMapper {
+
+    public static PatientTO mapToTO(PatientEntity patientEntity) {
+        if (patientEntity == null) {
+            return null;
+        }
+        PatientTO patientTO = new PatientTO();
+        patientTO.setId(patientEntity.getId());
+        patientTO.setFirstName(patientEntity.getFirstName());
+        patientTO.setLastName(patientEntity.getLastName());
+        patientTO.setTelephoneNumber(patientEntity.getTelephoneNumber());
+        patientTO.setEmail(patientEntity.getEmail());
+        patientTO.setPatientNumber(patientEntity.getPatientNumber());
+        patientTO.setDateOfBirth(patientEntity.getDateOfBirth());
+        patientTO.setVisits(patientEntity.getVisits().stream()
+                .map(VisitMapper::mapToTO)
+                .collect(Collectors.toList()));
+        // Uzupełnij dodatkowe pole
+        patientTO.setAdditionalField(patientEntity.getAdditionalField());
+        return patientTO;
+    }
+
+    public static PatientEntity mapToEntity(PatientTO patientTO) {
+        if (patientTO == null) {
+            return null;
+        }
+        PatientEntity patientEntity = new PatientEntity();
+        patientEntity.setId(patientTO.getId());
+        patientEntity.setFirstName(patientTO.getFirstName());
+        patientEntity.setLastName(patientTO.getLastName());
+        patientEntity.setTelephoneNumber(patientTO.getTelephoneNumber());
+        patientEntity.setEmail(patientTO.getEmail());
+        patientEntity.setPatientNumber(patientTO.getPatientNumber());
+        patientEntity.setDateOfBirth(patientTO.getDateOfBirth());
+        // Uzupełnij dodatkowe pole
+        patientEntity.setAdditionalField(patientTO.getAdditionalField());
+        return patientEntity;
+    }
+}
---
+++ wsb_jpa-master-Dawid_Gulka/wsb_jpa-master/wsb-jpa1/src/main/java/com/capgemini/wsb/persistence/entity/VisitEntity.java
@@ -0,0 +1,68 @@
+package com.capgemini.wsb.persistence.entity;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "VISIT")
+public class VisitEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String description;
+
+	@Column(nullable = false)
+	private LocalDateTime time;
+
+	@ManyToOne
+	@JoinColumn(name = "doctor_id", nullable = false)
+	private DoctorEntity doctor;
+
+	@ManyToOne
+	@JoinColumn(name = "patient_id", nullable = false)
+	private PatientEntity patient;
+
+	// Gettery i Settery
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public LocalDateTime getTime() {
+		return time;
+	}
+
+	public void setTime(LocalDateTime time) {
+		this.time = time;
+	}
+
+	public DoctorEntity getDoctor() {
+		return doctor;
+	}
+
+	public void setDoctor(DoctorEntity doctor) {
+		this.doctor = doctor;
+	}
+
+	public PatientEntity getPatient() {
+		return patient;
+	}
+
+	public void setPatient(PatientEntity patient) {
+		this.patient = patient;
+	}
+}
